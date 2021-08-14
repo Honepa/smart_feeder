480cc2595b83293524e398d962888e56e4038cfb
Honepa <cot1884@gmail.com>
2021-08-14 16:18:59 +0500

    gpio run

---
+++ raspi_test/gpio_knok_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 14 16:14:06 2021
+
+@author: honepa
+"""
+
+import gpiozero
+
+b1 = gpiozero.LED(27)
+b2 = gpiozero.LED(22)
+
+b2.off()
+b1.off()
+
+knok = gpiozero.Button("GPIO17")
+
+k = 0
+
+while k < 10:
+    b1.on()
+    if !knok.value:
+        k += 1
+b1.off()
